206e20f666bbfee148b7cc494697ae705e5c9f08
Huy Ngo <huybngo@yahoo.com>
2024-06-29 17:09:33 -0700

    Update index.ts

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { default as listRoutes } from './routes/list-vins';
+import { default as lookupRoutes } from './routes/lookup';
+import { default as removeRoutes } from './routes/remove-vin';
 import prismaClient from './db/prisma-client';
 
 const port = 3001;
@@ -12,6 +14,8 @@ app.use(express.json()); // JSON body parser middleware
 prismaClient.vin.deleteMany().then(count => {});
 
 app.use('/list', listRoutes);
+app.use('/lookup', lookupRoutes);
+app.use('/remove', removeRoutes);
 
 app.listen(port, (): void => {
   console.log('SERVER IS UP ON PORT:', port);
